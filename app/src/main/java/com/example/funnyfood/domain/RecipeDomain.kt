feac7ea71864a0843d7918fecb812e38ff0779bd
Adminius <vnakvasin@yandex.ru>
2022-08-18 14:46:54 +0700

    add RecipeDomain.kt

--- app/src/main/java/com/example/funnyfood/domain/RecipeDomain.kt
+++ app/src/main/java/com/example/funnyfood/domain/RecipeDomain.kt
@@ -4,3 +4,15 @@ import com.example.funnyfood.core.Abstract
 import com.example.funnyfood.ui.RecipeUi
 
 sealed class RecipeDomain : Abstract.Object<RecipeUi, RecipeDomainToUiMapper>
+
+    data class Base(
+        private val id: Int,
+        private val name: String,
+        private val urlImg: String,
+        private val cookingTime: String
+    ) : RecipeDomain() {
+        override fun map(mapper: RecipeDomainToUiMapper): RecipeUi {
+            return mapper.map(id, name, urlImg, cookingTime)
+        }
+
+    }
