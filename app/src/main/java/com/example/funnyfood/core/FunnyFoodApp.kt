31b6e5e1bb35b3dfdfa30985490fdb3b0b80a75a
Adminius <vnakvasin@yandex.ru>
2022-08-18 12:18:57 +0700

    fix FunnyFoodApp.kt

--- app/src/main/java/com/example/funnyfood/core/FunnyFoodApp.kt
+++ app/src/main/java/com/example/funnyfood/core/FunnyFoodApp.kt
@@ -2,10 +2,16 @@ package com.example.funnyfood.core
 
 import android.app.Application
 import android.util.Log
+import com.example.funnyfood.data.*
+import com.example.funnyfood.data.cache.RealmProvider
+import com.example.funnyfood.data.cache.RecipesCacheDataSource
 import com.example.funnyfood.data.cloud.RecipeCloudDataSource
 import com.example.funnyfood.data.cloud.RecipeMockServer
 import com.example.funnyfood.data.cloud.RecipeService
+import com.example.funnyfood.domain.BaseRecipesToDomainMapper
+import com.example.funnyfood.domain.RecipesInteractor
 import com.example.funnyfood.ui.MainViewModel
+import com.example.funnyfood.ui.RecipesCommunication
 import com.google.gson.Gson
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -27,7 +33,7 @@ class FunnyFoodApp: Application() {
     override fun onCreate() {
         super.onCreate()
         Realm.init(this)
-        mainViewModel = MainViewModel(applicationContext)
+
         val recipeMockServer = RecipeMockServer.Base()
         recipeMockServer.startServer("recipe.json",this)
         val client = OkHttpClient.Builder()
@@ -49,9 +55,28 @@ class FunnyFoodApp: Application() {
         val gson = Gson()
         val cloudDataSource = RecipeCloudDataSource.Base(service, gson)
         Log.i("MyTag", "${recipeMockServer.getBaseUrlMockServer()} before init retrofit")
+
+        val caheDataSource = RecipesCacheDataSource.Base(RealmProvider.Base(), RecipeDataToDbMapper.Base())
+        val toRecipeMapper = ToRecipeDataMapper.Base()
+        val recipesCloudMapper = RecipesCloudMapper.Base(toRecipeMapper)
+        val recipesCacheMapper = RecipesCacheMapper.Base(toRecipeMapper)
+        val recipesRepository = RecipesRepository.Base(
+            cloudDataSource,
+            caheDataSource,
+            recipesCloudMapper,
+            recipesCacheMapper
+        )
+        val recipesInteractor = RecipesInteractor.Base(
+            recipesRepository,
+            BaseRecipesToDomainMapper(BaseRecipeToDomainMapper())
+            )
+        val communication = RecipesCommunication.Base()
+        val resourceProvider = ResourceProvider.Base(this)
+        mainViewModel = MainViewModel(
+            recipesInteractor,
+            BaseRecipesDomainToUiMapper(resourceProvider, BaseRecipeDomainToUiMapper(resourceProvider)),
+            communication
+        )
     }
-
-
-
 
 }
--- app/src/main/java/com/example/funnyfood/data/RecipesCacheMapper.kt
+++ app/src/main/java/com/example/funnyfood/data/RecipesCacheMapper.kt
@@ -7,7 +7,7 @@ import com.example.funnyfood.data.cache.RecipeRealm
 interface RecipesCacheMapper : Abstract.Mapper {
     fun map(recipeDdList: List<Abstract.Object<RecipeData, ToRecipeDataMapper>>): List<RecipeData>
 
-    class Base(private val recipeDbList: List<RecipeDB>, private val mapper: ToRecipeDataMapper) :
+    class Base(private val mapper: ToRecipeDataMapper) :
         RecipesCacheMapper {
         override fun map(recipeDbList: List<Abstract.Object<RecipeData, ToRecipeDataMapper>>): List<RecipeData> {
             return recipeDbList.map { recipeDB -> recipeDB.map(mapper) }
