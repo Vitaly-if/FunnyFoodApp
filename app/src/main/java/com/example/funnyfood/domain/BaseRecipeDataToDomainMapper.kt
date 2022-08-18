444b8cbc184f669258ce7ca0ec3f444f355df9a1
Adminius <vnakvasin@yandex.ru>
2022-08-18 14:54:06 +0700

    fix BaseRecipeDataToDomainMapper.kt

--- app/src/main/java/com/example/funnyfood/domain/BaseRecipeDataToDomainMapper.kt
+++ app/src/main/java/com/example/funnyfood/domain/BaseRecipeDataToDomainMapper.kt
@@ -5,6 +5,6 @@ import com.example.funnyfood.data.RecipeDataToDomainMapper
 
 class BaseRecipeDataToDomainMapper : RecipeDataToDomainMapper {
     override fun map(id: Int, name: String, urlImg: String, cookingTime: String): RecipeDomain {
-        return RecipeDomain(id, name, urlImg, cookingTime)
+        return RecipeDomain.Base(id, name, urlImg, cookingTime)
     }
 }
