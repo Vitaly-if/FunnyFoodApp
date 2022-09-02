ca8c0c1154e24c3e1fe89198a1b1dbe25b01e186
Adminius <vnakvasin@yandex.ru>
2022-09-02 15:45:52 +0700

    fix MockServer

--- app/src/main/java/com/example/funnyfood/core/FunnyFoodApp.kt
+++ app/src/main/java/com/example/funnyfood/core/FunnyFoodApp.kt
@@ -1,12 +1,10 @@
 package com.example.funnyfood.core
 
 import android.app.Application
-import android.util.Log
 import com.example.funnyfood.data.*
 import com.example.funnyfood.data.cache.RealmProvider
 import com.example.funnyfood.data.cache.RecipesCacheDataSource
 import com.example.funnyfood.data.cloud.RecipeCloudDataSource
-import com.example.funnyfood.data.cloud.RecipeMockServer
 import com.example.funnyfood.data.cloud.RecipeService
 import com.example.funnyfood.domain.BaseRecipeDataToDomainMapper
 import com.example.funnyfood.domain.BaseRecipesDataToDomainMapper
@@ -19,20 +17,17 @@ import java.util.concurrent.TimeUnit
 import io.realm.Realm
 import retrofit2.Retrofit
 
-class FunnyFoodApp: Application() {
+class FunnyFoodApp : Application() {
 
     private companion object {
-        const val  BASE_URL = "https://bible-go-api.rkeplin.com/v1/"
+        const val BASE_URL = "https://bible-go-api.rkeplin.com/v1/"
     }
-
     lateinit var mainViewModel: MainViewModel
 
     override fun onCreate() {
         super.onCreate()
         Realm.init(this)
-
-        val recipeMockServer = RecipeMockServer.Base()
-        recipeMockServer.startServer("recipe.json",this)
+        val useMocks = true
         val client = OkHttpClient.Builder()
             .connectTimeout(1, TimeUnit.MINUTES)
             .readTimeout(1, TimeUnit.MINUTES)
@@ -40,33 +35,36 @@ class FunnyFoodApp: Application() {
                 level = HttpLoggingInterceptor.Level.BODY
             })
             .build()
-        Thread.sleep(500)
-       // Log.i("MyTag", "${recipeMockServer.getBaseUrlMockServer()} after init retrofit")
+
         var retrofit = Retrofit.Builder()
-            .baseUrl(recipeMockServer.getBaseUrlMockServer())
+            .baseUrl(BASE_URL)
             .client(client)
             .build()
 
         val service = retrofit.create(RecipeService::class.java)
 
         val gson = Gson()
-        val cloudDataSource = RecipeCloudDataSource.Base(service, gson)
-        Log.i("MyTag", "${recipeMockServer.getBaseUrlMockServer()} before init retrofit")
 
-        val caheDataSource = RecipesCacheDataSource.Base(RealmProvider.Base(), RecipeDataToDbMapper.Base())
+        val cloudDataSource = if (useMocks)
+            RecipeCloudDataSource.Mock(resources, gson)
+        else
+            RecipeCloudDataSource.Base(service, gson)
+
+        val cacheDataSource =
+            RecipesCacheDataSource.Base(RealmProvider.Base(), RecipeDataToDbMapper.Base())
         val toRecipeMapper = ToRecipeDataMapper.Base()
         val recipesCloudMapper = RecipesCloudMapper.Base(toRecipeMapper)
         val recipesCacheMapper = RecipesCacheMapper.Base(toRecipeMapper)
         val recipesRepository = RecipesRepository.Base(
             cloudDataSource,
-            caheDataSource,
+            cacheDataSource,
             recipesCloudMapper,
             recipesCacheMapper
         )
         val recipesInteractor = RecipesInteractor.Base(
             recipesRepository,
             BaseRecipesDataToDomainMapper(BaseRecipeDataToDomainMapper())
-            )
+        )
         val communication = RecipesCommunication.Base()
         val resourceProvider = ResourceProvider.Base(this)
         mainViewModel = MainViewModel(
--- app/src/main/java/com/example/funnyfood/data/cloud/RecipeCloudDataSource.kt
+++ app/src/main/java/com/example/funnyfood/data/cloud/RecipeCloudDataSource.kt
@@ -1,19 +1,42 @@
 package com.example.funnyfood.data.cloud
 
+import android.content.res.Resources
+import com.example.funnyfood.R
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import java.io.BufferedReader
 
 interface RecipeCloudDataSource {
 
     suspend fun fetchRecipes(): List<RecipeDTO>
 
+    abstract class Abstract(private val gson: Gson) : RecipeCloudDataSource {
+        override suspend fun fetchRecipes(): List<RecipeDTO> = gson.fromJson(
+            getDataAsString(),
+            object : TypeToken<List<RecipeDTO>>() {}.type
+        )
+
+
+        protected abstract suspend fun getDataAsString(): String
+    }
+
+
     class Base(
         private val service: RecipeService,
-        private val gson: Gson
-    ) : RecipeCloudDataSource {
-        private val type = object : TypeToken<List<RecipeDTO>>() {}.type
-        override suspend fun fetchRecipes(): List<RecipeDTO> {
-            return gson.fromJson(service.fetchRecipes().string(), type)
-        }
+        gson: Gson
+    ) : RecipeCloudDataSource.Abstract(gson) {
+        override suspend fun getDataAsString() = service.fetchRecipes().string()
+
+    }
+
+    class Mock(
+        private val resources: Resources,
+        gson: Gson
+    ) : RecipeCloudDataSource.Abstract(gson) {
+        override suspend fun getDataAsString() =
+            resources.openRawResource(R.raw.recipes).bufferedReader()
+                .use(BufferedReader::readText)
+
+
     }
 }
