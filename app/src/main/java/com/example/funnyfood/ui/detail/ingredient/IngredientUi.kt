0c83832ce072228ea5a0bd77fdec5f83182fb74d
Adminius <vnakvasin@yandex.ru>
2022-10-19 15:35:12 +0700

    fix RecipeDetailUI IngredientUi.kt fragment_detail.xml

--- app/src/main/java/com/example/funnyfood/ui/detail/ingredient/IngredientUi.kt
+++ app/src/main/java/com/example/funnyfood/ui/detail/ingredient/IngredientUi.kt
@@ -1,12 +1,18 @@
 package com.example.funnyfood.ui.detail.ingredient
 
+import android.widget.TextView
 import com.example.funnyfood.core.Abstract
 
 data class IngredientUi(
     private val name: String, private val quantity: String,
     private val unit: String
-): Abstract.Object<Unit, Abstract.Mapper.Empty> {
+) : Abstract.Object<Unit, Abstract.Mapper.Empty> {
     override fun map(mapper: Abstract.Mapper.Empty) {
         //todo fix empty mapper
+    }
+
+    fun show(ingredientName: TextView, ingredientAmount: TextView) {
+        ingredientName.text = name
+        ingredientAmount.text = quantity
     }
 }
