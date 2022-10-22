6749fc916db7cbfb7d265e346a123df64545aac9
Adminius <vnakvasin@yandex.ru>
2022-10-22 18:51:36 +0700

    fix BaseAdapter.kt BaseFragment.kt

--- app/src/main/java/com/example/funnyfood/ui/BaseFragment.kt
+++ app/src/main/java/com/example/funnyfood/ui/BaseFragment.kt
@@ -1,12 +1,9 @@
 package com.example.funnyfood.ui
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
-import com.example.funnyfood.R
 
 abstract class BaseFragment : Fragment() {
 
@@ -14,17 +11,9 @@ abstract class BaseFragment : Fragment() {
 
     protected abstract fun getTitle(): String
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_main, container, false)
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         requireActivity().title = getTitle()
-        recyclerView = view.findViewById(R.id.recyclerView)
+
     }
 }
