9b117de52932592c4ce176d80d3d112485fc3faa
Adminius <vnakvasin@yandex.ru>
2022-10-23 20:05:34 +0700

    add Toolbar add onBackPress

--- app/src/main/java/com/example/funnyfood/MainActivity.kt
+++ app/src/main/java/com/example/funnyfood/MainActivity.kt
@@ -2,9 +2,14 @@ package com.example.funnyfood
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.lifecycle.ViewModel
 import com.example.funnyfood.core.FunnyFoodApp
 import com.example.funnyfood.databinding.ActivityMainBinding
+import com.example.funnyfood.ui.MainViewModel
 import com.example.funnyfood.ui.Navigator.Base.Screens.Companion.RECIPE_DETAIL_SCREEN
 import com.example.funnyfood.ui.Navigator.Base.Screens.Companion.RECIPE_LIST_SCREEN
 import com.example.funnyfood.ui.detail.RecipeDetailFragment
@@ -12,12 +17,16 @@ import com.example.funnyfood.ui.recipes.RecipesFragment
 import java.lang.IllegalStateException
 
 class MainActivity : AppCompatActivity() {
+    lateinit var viewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         val binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
         val view = binding.root
         setContentView(view)
-        val viewModel = (application as FunnyFoodApp).mainViewModel
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        viewModel = (application as FunnyFoodApp).mainViewModel
         viewModel.observe(this) {
             val fragment = when (it) {
                 RECIPE_LIST_SCREEN -> RecipesFragment()
@@ -31,6 +40,27 @@ class MainActivity : AppCompatActivity() {
 
         viewModel.init()
 
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            onBackPressed()
+            return true
+        }
+        if (item.itemId == R.id.menu) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onBackPressed() {
+        if (viewModel.navigateBack())
+            super.onBackPressed()
     }
 
 }
--- app/src/main/java/com/example/funnyfood/ui/detail/RecipeDetailFragment.kt
+++ app/src/main/java/com/example/funnyfood/ui/detail/RecipeDetailFragment.kt
@@ -1,33 +1,64 @@
 package com.example.funnyfood.ui.detail
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import com.example.funnyfood.R
 import com.example.funnyfood.core.FunnyFoodApp
+import com.example.funnyfood.databinding.FragmentDetailBinding
 import com.example.funnyfood.ui.BaseFragment
+import com.example.funnyfood.ui.detail.cookingstep.CookingStepAdapter
+import com.example.funnyfood.ui.detail.ingredient.IngredientAdapter
 
-class RecipeDetailFragment: BaseFragment() {
+class RecipeDetailFragment : BaseFragment() {
 
     private lateinit var viewModel: RecipeDetailViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewModel = (requireActivity().application as FunnyFoodApp.resipeDetailViewModel)
+
+        viewModel = (requireActivity().application as FunnyFoodApp).recipeDetailViewModel
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        (requireActivity() as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(
+            true
+        )
+
+
+
+        return inflater.inflate(R.layout.fragment_detail, container, false)
     }
 
     override fun getTitle(): String {
-        return "Detail" // todo fix getTitle DetailFragment
+        return "Рецепт" // todo fix getTitle DetailFragment
     }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val adapter = RecipeDetailAdapter()
+        viewModel.fetchRecipe()
 
-        recyclerView?.adapter = adapter
+        val binding = FragmentDetailBinding.bind(view)
+        val adapterIngredient = IngredientAdapter()
+        val adapterCookingStep = CookingStepAdapter()
 
-        viewModel.observe(this, Observer{
-            adapter.setData(it)
+
+        viewModel.observe(this, Observer {
+            it.forEach {
+                it.show(binding, adapterIngredient, adapterCookingStep)
+
+            }
         })
+        viewModel.init()
     }
 }
