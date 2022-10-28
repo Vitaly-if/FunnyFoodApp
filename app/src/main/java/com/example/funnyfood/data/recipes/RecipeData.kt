17a394ef55b7f8c22b74b609e81ba1788a0264db
Adminius <vnakvasin@yandex.ru>
2022-10-28 16:51:10 +0700

    fix RecipeData

--- app/src/main/java/com/example/funnyfood/data/recipes/RecipeData.kt
+++ app/src/main/java/com/example/funnyfood/data/recipes/RecipeData.kt
@@ -1,26 +1,24 @@
 package com.example.funnyfood.data.recipes
 
 import com.example.funnyfood.core.Abstract
-import com.example.funnyfood.data.recipes.cache.DbWrapper
+import com.example.funnyfood.core.DbWrapper
 import com.example.funnyfood.data.recipes.cache.RecipeDB
 import com.example.funnyfood.domain.recipes.RecipeDomain
+import io.realm.RealmObject
 
 data class RecipeData(
     private val id: Int,
     private val name: String,
     private val urlImg: String,
     private val cookingTime: String
-) : ToRecipeDb<RecipeDB, RecipeDataToDbMapper>,
+) : Abstract.Mapper.ToDb<RecipeDB, RecipeDataToDbMapper>,
     Abstract.Object<RecipeDomain, RecipeDataToDomainMapper> {
+
     override fun map(mapper: RecipeDataToDomainMapper): RecipeDomain {
         return mapper.map(id, name, urlImg, cookingTime)
     }
 
-    override fun mapTo(mapper: RecipeDataToDbMapper, db: DbWrapper): RecipeDB {
+    override fun mapBy(mapper: RecipeDataToDbMapper, db: DbWrapper<RecipeDB>): RecipeDB {
         return mapper.mapToDb(id, name, urlImg, cookingTime, db)
     }
-}
-
-interface ToRecipeDb<T, M : Abstract.Mapper> {
-    fun mapTo(mapper: M, db: DbWrapper): T
 }
