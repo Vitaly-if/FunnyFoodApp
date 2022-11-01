66a6a723794d95c591056e67c65da012f2b5ecb8
Adminius <vnakvasin@yandex.ru>
2022-11-01 15:15:48 +0700

    fix Abstract.kt CoreModule.kt

--- app/src/main/java/com/example/funnyfood/data/recipes/RecipeDataToDbMapper.kt
+++ app/src/main/java/com/example/funnyfood/data/recipes/RecipeDataToDbMapper.kt
@@ -1,28 +1,26 @@
 package com.example.funnyfood.data.recipes
 
 import com.example.funnyfood.core.Abstract
-import com.example.funnyfood.core.DbWrapper
 import com.example.funnyfood.data.recipes.cache.RecipeDB
 
 interface RecipeDataToDbMapper : Abstract.Mapper {
 
-    fun mapToDb(
+    fun mapBy(
         id: Int,
         name: String,
         urlImg: String,
-        cookingTime: String,
-        db: DbWrapper<RecipeDB>
+        cookingTime: String
     ): RecipeDB
 
     class Base : RecipeDataToDbMapper {
-        override fun mapToDb(
+        override fun mapBy(
             id: Int,
             name: String,
             urlImg: String,
-            cookingTime: String,
-            db: DbWrapper<RecipeDB>
+            cookingTime: String
         ): RecipeDB {
-            val recipeDB = db.createObject(id)
+            val recipeDB = RecipeDB()
+            recipeDB.id = id
             recipeDB.name = name
             recipeDB.urlImg = urlImg
             recipeDB.cookingTime = cookingTime
