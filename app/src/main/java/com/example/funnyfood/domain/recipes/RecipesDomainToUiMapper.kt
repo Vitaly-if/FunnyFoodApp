fe041f0ad71c02aa236c9784739539b1becc7b68
Adminius <vnakvasin@yandex.ru>
2022-11-07 22:49:00 +0700

    fix BaseRecipesDomainToUiMapper.kt RecipesDomainToUiMapper.kt

--- app/src/main/java/com/example/funnyfood/domain/recipes/RecipesDomainToUiMapper.kt
+++ app/src/main/java/com/example/funnyfood/domain/recipes/RecipesDomainToUiMapper.kt
@@ -1,10 +1,8 @@
 package com.example.funnyfood.domain.recipes
 
 import com.example.funnyfood.core.Abstract
-import com.example.funnyfood.domain.ErrorType
+import com.example.funnyfood.ui.ResourceProvider
 import com.example.funnyfood.ui.recipes.RecipesUi
 
-interface RecipesDomainToUiMapper: Abstract.Mapper {
-    fun map(list: List<RecipeDomain>): RecipesUi
-    fun map(errorType: ErrorType): RecipesUi
-}
+abstract class RecipesDomainToUiMapper(resourceProvider: ResourceProvider):
+    Abstract.Mapper.DomainToUi.Base<List<RecipeDomain>, RecipesUi>(resourceProvider)
