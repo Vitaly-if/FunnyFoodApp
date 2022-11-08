59ac64d574945b9ff5d26c77c08916188b00cacf
Adminius <vnakvasin@yandex.ru>
2022-11-08 21:33:59 +0700

    fix RecipeUi.kt RecipesAdapter.kt RecipeViewHolder.kt

--- app/src/main/java/com/example/funnyfood/ui/recipes/RecipeUi.kt
+++ app/src/main/java/com/example/funnyfood/ui/recipes/RecipeUi.kt
@@ -1,33 +1,32 @@
 package com.example.funnyfood.ui.recipes
 
-import android.widget.ArrayAdapter
+import android.view.View
 import android.widget.ImageView
-import android.widget.TextView
 import com.example.funnyfood.R
+import com.example.funnyfood.databinding.RecipeItemErrorBinding
+import com.example.funnyfood.databinding.RecipeItemLayoutBinding
 
 sealed class RecipeUi {
-    open fun show(
-        textContainer: TextView,
-        textContainerTwo: TextView,
-        imageContainer: ImageContainer
-    ) {
-    }
 
-    open fun show(text: TextView, textTwo: TextView, image: ImageView) = Unit
     open fun open(recipeListener: RecipesAdapter.RecipeListener) = Unit
+
+    abstract fun show(view: View)
+
     class Base(
         private val id: Int,
         private val name: String,
         private val urlImage: String,
-        private val cookingTime: String
+        private val cookingTime: String,
     ) :
         RecipeUi() {
 
-        override fun show(text: TextView, textTwo: TextView, imageContainer: ImageView) {
-            text.text = name
-            textTwo.text = cookingTime
-            imageContainer.setImageResource(R.drawable.rec_img_01)
-
+        override fun show(view: View) {
+            val binding = RecipeItemLayoutBinding.bind(view)
+            binding.apply {
+                nameTextView.text = name
+                timeTextView.text = cookingTime
+                recipeImageView.setImageResource(R.drawable.rec_img_01)
+            }
         }
 
         override fun open(recipeListener: RecipesAdapter.RecipeListener) {
@@ -35,7 +34,12 @@ sealed class RecipeUi {
         }
     }
 
-    class Fail(private val message: String) : RecipeUi()
+    class Fail(private val message: String) : RecipeUi() {
+        override fun show(view: View) {
+            val binding = RecipeItemErrorBinding.bind(view)
+            binding.errorMessage.text = message
+        }
+    }
 
     interface TextContainer {
 
--- app/src/main/java/com/example/funnyfood/ui/recipes/RecipesAdapter.kt
+++ app/src/main/java/com/example/funnyfood/ui/recipes/RecipesAdapter.kt
@@ -6,7 +6,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.funnyfood.R
 
 class RecipesAdapter(
-    private val recipeListener: RecipeListener
+    private val recipeListener: RecipeListener,
 ) : RecyclerView.Adapter<RecipeBaseViewHolder>() {
 
     private val items = ArrayList<RecipeUi>()
@@ -17,12 +17,20 @@ class RecipesAdapter(
         notifyDataSetChanged()
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipeBaseViewHolder {
-
-        return RecipeViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.recipe_item_layout, parent, false)
-        )
+    override fun getItemViewType(position: Int) = when (items[position]) {
+        is RecipeUi.Base -> 0
+        is RecipeUi.Fail -> 1
     }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipeBaseViewHolder =
+        when (viewType) {
+            0 -> RecipeViewHolder(LayoutInflater.from(parent.context)
+                .inflate(R.layout.recipe_item_layout, parent, false))
+            1 -> FailRecipeVewHolder(LayoutInflater.from(parent.context)
+                .inflate(R.layout.recipe_item_error, parent, false))
+            else -> RecipeViewHolder(LayoutInflater.from(parent.context)
+                .inflate(R.layout.recipe_item_layout, parent, false))
+        }
 
     override fun onBindViewHolder(holder: RecipeBaseViewHolder, position: Int) {
         holder.onBind(items[position], recipeListener)
