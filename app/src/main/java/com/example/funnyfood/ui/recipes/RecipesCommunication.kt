60c73050f4b4a1b49b7f22684353f692d11c3ba2
Adminius <vnakvasin@yandex.ru>
2022-11-09 13:51:50 +0700

    fix RecipesCommunication.kt

--- app/src/main/java/com/example/funnyfood/ui/recipes/RecipesCommunication.kt
+++ app/src/main/java/com/example/funnyfood/ui/recipes/RecipesCommunication.kt
@@ -1,22 +1,7 @@
 package com.example.funnyfood.ui.recipes
 
-import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Observer
-import com.example.funnyfood.core.Abstract
+import com.example.funnyfood.core.Communication
 
-interface RecipesCommunication : Abstract.Mapper {
-    fun map(recipesUi: List<RecipeUi>)
-    fun observe(owner: LifecycleOwner, observer: Observer<List<RecipeUi>>)
-
-    class Base : RecipesCommunication {
-        private val listLiveData = MutableLiveData<List<RecipeUi>>()
-        override fun map(recipesUi: List<RecipeUi>) {
-            listLiveData.value = recipesUi
-        }
-
-        override fun observe(owner: LifecycleOwner, observer: Observer<List<RecipeUi>>) {
-            listLiveData.observe(owner, observer)
-        }
-    }
+interface RecipesCommunication : Communication<List<RecipeUi>> {
+    class Base : Communication.Base<List<RecipeUi>>() , RecipesCommunication
 }
