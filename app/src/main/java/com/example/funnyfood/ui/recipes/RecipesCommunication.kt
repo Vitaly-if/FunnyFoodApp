b8d2d542fdd5e8a7ad404de13df327bc38cd22f1
Adminius <vnakvasin@yandex.ru>
2022-11-09 13:52:02 +0700

    fix RecipesCommunication.kt

--- app/src/main/java/com/example/funnyfood/ui/recipes/RecipesCommunication.kt
+++ app/src/main/java/com/example/funnyfood/ui/recipes/RecipesCommunication.kt
@@ -3,5 +3,5 @@ package com.example.funnyfood.ui.recipes
 import com.example.funnyfood.core.Communication
 
 interface RecipesCommunication : Communication<List<RecipeUi>> {
-    class Base : Communication.Base<List<RecipeUi>>() , RecipesCommunication
+    class Base : Communication.Base<List<RecipeUi>>(), RecipesCommunication
 }
