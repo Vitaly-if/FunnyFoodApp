76d920526fc5f67cda32f5025ce9ba5524a47229
Adminius <vnakvasin@yandex.ru>
2022-11-14 15:44:49 +0700

    fix RecipeUi.kt add Glide

--- app/src/main/java/com/example/funnyfood/ui/recipes/RecipeUi.kt
+++ app/src/main/java/com/example/funnyfood/ui/recipes/RecipeUi.kt
@@ -2,9 +2,11 @@ package com.example.funnyfood.ui.recipes
 
 import android.view.View
 import android.widget.ImageView
+import com.bumptech.glide.Glide
 import com.example.funnyfood.R
 import com.example.funnyfood.databinding.RecipeItemErrorBinding
 import com.example.funnyfood.databinding.RecipeItemLayoutBinding
+import com.example.funnyfood.ui.ResourceProvider
 
 sealed class RecipeUi {
 
@@ -25,10 +27,13 @@ sealed class RecipeUi {
             binding.apply {
                 nameTextView.text = name
                 timeTextView.text = cookingTime
-                recipeImageView.setImageResource(R.drawable.rec_img_01)
+                println(urlImage)
+                recipeImageView.load(urlImage)
             }
         }
-
+        private fun ImageView.load(url: String) {
+            Glide.with(this).load(url).error(R.drawable.ic_broken_image).into(this)
+        }
         override fun open(recipeListener: RecipesAdapter.RecipeListener) {
             recipeListener.showRecipe(id)
         }
